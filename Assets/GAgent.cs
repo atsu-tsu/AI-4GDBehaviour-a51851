6408e3c853f3c22c6939b65cdc9203e4592a3b2e
atsu-tsu <fdferreira93@gmail.com>
2023-11-22 16:48:03 +0000

    1. Agents

--- Assets/GAgent.cs
+++ Assets/GAgent.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+
+public class SubGoal
+{
+
+    public Dictionary<string, int> sGoals;
+    public bool remove;
+
+    public SubGoal(string s, int i, bool r)
+    {
+
+        sGoals = new Dictionary<string, int>();
+        sGoals.Add(s, i);
+        remove = r;
+    }
+}
 
 public class GAgent : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public List<GAction> actions = new List<GAction>();
+    public Dictionary<SubGoal, int> goals = new Dictionary<SubGoal, int>();
+    public WorldStates beliefs = new WorldStates();
+
+    GPlanner planner;
+    Queue<GAction> actionQueue;
+    public GAction currentAvtion;
+    SubGoal currentGoal;
+
+    
     void Start()
     {
-        
+
+        GAction[] acts = this.GetComponents<GAction>();
+        foreach (GAction a in acts)
+        {
+
+            actions.Add(a);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+
     }
 }
