486ffcd6d2e576a17e7a15c99461dac9b2abe68f
atsu-tsu <fdferreira93@gmail.com>
2023-11-23 00:48:08 +0000

    1. Executing a Simple Plan

--- Assets/GAction.cs
+++ Assets/GAction.cs
@@ -3,59 +3,67 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public abstract class GAction : MonoBehaviour
-{
+public abstract class GAction : MonoBehaviour {
+
     public string actionName = "Action";
     public float cost = 1.0f;
     public GameObject target;
-    public GameObject targetTag;
-    public float duration = 0;
+    public string targetTag;
+    public float duration = 0.0f;
     public WorldState[] preConditions;
     public WorldState[] afterEffects;
     public NavMeshAgent agent;
-
     public Dictionary<string, int> preconditions;
     public Dictionary<string, int> effects;
-
     public WorldStates agentBeliefs;
-
     public bool running = false;
 
-    public GAction()
-    {
+    public GAction() {
+
+        // Set up the preconditions and effects
         preconditions = new Dictionary<string, int>();
         effects = new Dictionary<string, int>();
     }
 
-    public void Awake()
-    {
+    private void Awake() {
+
+        // Get hold of the agents NavMeshAgent
         agent = this.gameObject.GetComponent<NavMeshAgent>();
 
-        if (preConditions != null)
-            foreach (WorldState w in preConditions)
-            {
+        // Check validity of preConditions
+        if (preConditions != null) {
+
+            foreach (WorldState w in preConditions) {
+
+                // Add each item to our Dictionary
                 preconditions.Add(w.key, w.value);
             }
+        }
 
-        if (afterEffects != null)
-            foreach (WorldState w in afterEffects)
-            {
+        // Check validity of afterEffects
+        if (afterEffects != null) {
+
+            foreach (WorldState w in afterEffects) {
+
+                // Add each item to our Dictionary
                 effects.Add(w.key, w.value);
             }
-
+        }
     }
 
-    public bool IsAchievable()
-    {
+    public bool IsAchievable() {
+
         return true;
     }
 
-    public bool IsAchievableGiven(Dictionary<string, int> conditions)
-    {
-        foreach (KeyValuePair<string, int> p in preconditions)
-        {
-            if (!conditions.ContainsKey(p.Key))
+    public bool IsAhievableGiven(Dictionary<string, int> conditions) {
+
+        foreach (KeyValuePair<string, int> p in preconditions) {
+
+            if (!conditions.ContainsKey(p.Key)) {
+
                 return false;
+            }
         }
         return true;
     }
